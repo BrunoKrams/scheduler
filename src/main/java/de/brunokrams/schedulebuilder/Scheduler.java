66b75a4ab2b7518e5cd1edd3eba142f95dd9e46a
Markus Born <markus.born-extern@deutschebahn.com>
2024-07-13 13:18:36 +0200

    Initial

--- src/main/java/de/brunokrams/schedulebuilder/Scheduler.java
+++ src/main/java/de/brunokrams/schedulebuilder/Scheduler.java
@@ -5,7 +5,9 @@ import java.util.Set;
 public class Scheduler<T> {
     
     public Schedule<T> create(Set<T> participants) {
+        if (participants.size() % 2 != 0) {
+            throw new IllegalArgumentException("Number of participants must be even.");
+        }
         return null;
     }
-    
 }
--- src/test/java/de/brunokrams/schedulebuilder/SchedulerTest.java
+++ src/test/java/de/brunokrams/schedulebuilder/SchedulerTest.java
@@ -7,37 +7,51 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static de.brunokrams.schedulebuilder.CommonTestData.EULER;
+import static de.brunokrams.schedulebuilder.CommonTestData.GAUSS;
 import static de.brunokrams.schedulebuilder.CommonTestData.PARTICIPANTS;
+import static de.brunokrams.schedulebuilder.CommonTestData.RIEMANN;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class SchedulerTest {
+
+    @Test
+    void create_fails_whenNumberOfParticipantsIsNotEven() {
+        // given
+        Scheduler<String> scheduler = new Scheduler<>();
+
+        // when/then
+        assertThatThrownBy(() -> scheduler.create(Set.of(GAUSS, RIEMANN, EULER))).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Number of participants must be even.");
+    }
 
     @Test
     void eachParticipant_playsEachOtherExactlyOnce() {
         // given
         Scheduler<String> scheduler = new Scheduler<>();
-    
+
         // when
         Schedule<String> schedule = scheduler.create(PARTICIPANTS);
-        
+
         // then
         for (String participant : PARTICIPANTS) {
             Set<String> expected = new HashSet<>(PARTICIPANTS);
             expected.remove(participant);
             assertThat(getOpponents(schedule, participant)).containsExactlyInAnyOrderElementsOf(expected);
         }
-    }   
-    
+    }
+
     private List<String> getOpponents(Schedule<String> schedule, String participant) {
         List<String> result = new ArrayList<>();
         for (Pairing<String> pairing : schedule.getPairings(participant)) {
             if (pairing.getFirstParticipant().equals(participant)) {
                 result.add(pairing.getSecondParticipant());
-            } else{
+            } else {
                 result.add(pairing.getFirstParticipant());
             }
         }
-        return result;        
+        return result;
     }
-    
+
 }
