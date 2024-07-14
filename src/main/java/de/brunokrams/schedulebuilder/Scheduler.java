2672c9808c9aa7177271129066ba050fd8322d89
Markus Born <born4501@uni-trier.de>
2024-07-14 20:01:43 +0200

    Blabla

--- src/main/java/de/brunokrams/schedulebuilder/Scheduler.java
+++ src/main/java/de/brunokrams/schedulebuilder/Scheduler.java
@@ -8,6 +8,14 @@ public class Scheduler<T>  {
         if (participants.size() % 2 != 0) {
             throw new IllegalArgumentException("Number of participants must be even.");
         }
+        if (participants.size() == 2) {
+            Schedule<T> schedule = new Schedule<>();
+            Round<T> round = new Round<>();
+            round.add(new Pairing<>(participants.get(0), participants.get(1)));
+            schedule.add(round);
+            return schedule;
+        }
+
         Schedule<T> schedule = new Schedule<>();
         Table<T> table = new Table<>(participants);
         for (int i = 0; i < participants.size() - 1; i++) {
